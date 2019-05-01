15e491bdf7e457c0997539f589b7e8ad882a6630
Jesse Lee <jesse@kinvite.co>
2019-04-30 23:09:08 -0400

    updating word count

--- wordcount.js
+++ wordcount.js
@@ -3,8 +3,6 @@ const wordcount = require('wordcount');
 
 const COMMENT_REGEX = /<!--[\S\s]*?-->/g;
 
-const NANO_COUNT = 21297;
-
 var total = 0;
 
 function countWords( filename ){
@@ -28,7 +26,6 @@ fs.readdir("./Chapters", function(err, items) {
 
   let now = new Date().toString();
   console.log(`\nTotal:\t\t\t${total} words`);
-  console.log(`Total Nano:\t\t${total - NANO_COUNT} words`)
 
   fs.appendFileSync("wordcount.log", `${now}\t\t${total} words\r\n`);
 });
